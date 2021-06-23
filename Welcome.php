b01324eb1f16c27a219cb8d0289bf35b851d12ce
JIT SAHA <43725078+Jit-Saha@users.noreply.github.com>
2021-06-23 14:38:41 +0530

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -13,11 +13,11 @@
 </head>
 <body>
 <?php
-/*session_start();
+session_start();
     if( !isset($_SESSION['loggedin']) || $_SESSION['loggedin']==false ){
         header('location: index.php');
         exit;
-    }*/
+    }
 ?>
 <div class="headW">
     <div class="HeadButton">
